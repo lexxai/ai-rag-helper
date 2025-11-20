96e49cd32214b5d4079afe282755a607911a5573
LeXXai <lexxai@gmail.com>
2025-11-20 20:09:08 +0200

    metrics check import

--- src/routers/metrics.py
+++ src/routers/metrics.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from fastapi.responses import Response
-from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
+
+try:
+    from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
+except ImportError:
+    generate_latest = None
+    CONTENT_TYPE_LATEST = None
 
 from config.logger import get_logger
 
@@ -12,4 +17,6 @@ router = APIRouter(prefix="/metrics", tags=["metrics"])
 # Prometheus metrics endpoint
 @router.get("/")
 async def metrics():
+    if generate_latest is None:
+        return Response(status_code=status.HTTP_501_NOT_IMPLEMENTED)
     return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
